0045e56037e5f4eb85a804972396460a119eae71
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-04 07:26:30 +0530

    Time: 35 ms (11.90%), Space: 13.8 MB (42.36%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,17 +1,27 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int>m1;
         for(int it:nums)m1[it]++;
         
-        priority_queue<pair<int,int>> p1;
+        // fit(m1)cout<<it.first<<" "<<it.second<<endl;
+        p<pair<int,int>> p1;
         for(auto it:m1)p1.emplace(it.second,it.first);
-        // priority_queue<pair<int,int>> p2=p1;
-        // while(!p2.empty()){
-        //     cout<<p2.top().first<<" "<<p2.top().second<<endl;
-        //     p2.pop();
-        // }
+        
+        // p<pair<int,int>> p2=p1;
+        
+        // while(!p2.empty()){cout<<p2.top().first<<" "<<p2.top().second<<endl;p2.pop();}
+        
+        
         nums.clear();
+        
         while(k--){
             nums.push_back(p1.top().second);
             p1.pop();
