a4da8a43db48b7a3aa679e843b4e4889976ff724
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-04 07:34:30 +0530

    Time: 7 ms (88.06%), Space: 9.8 MB (97.58%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -7,9 +7,9 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
-        nums=mergeSort(nums,0,nums.size()-1);
+        // nums=mergeSort(nums,0,nums.size()-1);
         // fit(nums)cout<<it<<endl;
-        cout<<nums[0];
+        // cout<<nums[0];
         return nums[nums.size()-k];
     }
     
@@ -18,31 +18,31 @@ public:
     //     return v1;
     // }
     
-    v<int> mergeSort(v<int>v1,int l,int r){
-        int m=l+(r-l)/2;
-        if(l<r){
-            mergeSort(v1,l,m);
-            mergeSort(v1,m+1,r);
-            merge(v1,l,m,r);
-        }
-        return v1;
-    }
-    v<int> merge(v<int>v1,int p,int q, int r){
-        int n1=q-p+1;
-        int n2=r-q;
-        int i=0,j=0,k=p;
-        v<int> l(n1),m(n2);
-        for(;i<n1;i++)l[i]=v1[p+i];
-        for(;j<n2;j++)m[j]=v1[q+1+j];
+//     v<int> mergeSort(v<int>v1,int l,int r){
+//         int m=l+(r-l)/2;
+//         if(l<r){
+//             mergeSort(v1,l,m);
+//             mergeSort(v1,m+1,r);
+//             merge(v1,l,m,r);
+//         }
+//         return v1;
+//     }
+//     v<int> merge(v<int>v1,int p,int q, int r){
+//         int n1=q-p+1;
+//         int n2=r-q;
+//         int i=0,j=0,k=p;
+//         v<int> l(n1),m(n2);
+//         for(;i<n1;i++)l[i]=v1[p+i];
+//         for(;j<n2;j++)m[j]=v1[q+1+j];
         
-        while(i<n1 && j<n2){
-            if(l[i]<m[j]){v1[k]=l[i];i++;}
-            else {v1[k]=m[j];j++;}
-            k++;
-        }
+//         while(i<n1 && j<n2){
+//             if(l[i]<m[j]){v1[k]=l[i];i++;}
+//             else {v1[k]=m[j];j++;}
+//             k++;
+//         }
         
-        while(i<n1){v1[k]=l[i];i++;k++;}
-        while(j<n2){v1[k]=m[k];j++;k++;}
-        return v1;
-    }
+//         while(i<n1){v1[k]=l[i];i++;k++;}
+//         while(j<n2){v1[k]=m[k];j++;k++;}
+//         return v1;
+//     }
 };
