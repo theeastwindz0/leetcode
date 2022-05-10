20c1ca16aff86514bd58642d20e8c2d8024383e9
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-10 08:41:17 +0530

    Time: 50 ms (31.00%), Space: 37.9 MB (21.41%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -14,18 +14,36 @@ class Solution {
 public:
     v<int>temp;
     v<v<int>>ans;
-    void helper(int i,int target,v<int>can){
-        if(target==0){ans.pb(temp);return;}
-        if(target<0 || i==can.size())return;
-        helper(i+1,target,can);
+//     void helper(int i,int target,v<int>can){
+//         if(target==0){ans.pb(temp);return;}
+//         if(target<0 || i==can.size())return;
+//         helper(i+1,target,can);
         
-        temp.pb(can[i]);
-        helper(i,target-can[i],can);
-        temp.ppb();
+//         temp.pb(can[i]);
+//         helper(i,target-can[i],can);
+//         temp.ppb();
+//     }
+    
+//     vector<vector<int>> combinationSum(vector<int>& can, int target) {
+//       helper(0,target,can);
+//         return ans;
+//     }
+    
+    void helper(int cInd,int sum,int target,v<int>can){
+        if(sum>target)return;
+        if(sum==target){ans.pb(temp);return;}
+        //If Cind replaced by 0 then it will ans will give all possible combination
+        f(cInd,can.size()){
+            temp.pb(can[i]);
+            sum+=can[i];
+            helper(i,sum,target,can);
+            temp.ppb();
+            sum-=can[i];
+        }
     }
     
-    vector<vector<int>> combinationSum(vector<int>& can, int target) {
-      helper(0,target,can);
+     vector<vector<int>> combinationSum(vector<int>& can, int target) {
+      helper(0,0,target,can);
         return ans;
     }
 };
