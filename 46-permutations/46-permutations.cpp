85185ad2a469731484988eadeaaa4608666b8301
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-10 17:56:33 +0530

    Time: 6 ms (37.86%), Space: 8.2 MB (17.51%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,28 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
+#define pb push_back
+#define ppb pop_back
+#define TN TreeNode
+#define LN ListNode
+class Solution {
+public:
+    v<v<int>> ans;
+    void solver(int cI,int n,v<int>nums){
+        if(cI==n){ans.pb(nums);return;}
+        f(cI,n+1){
+            swap(nums[i],nums[cI]);
+            solver(cI+1,n,nums);
+            swap(nums[i],nums[cI]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        solver(0,nums.size()-1,nums);
+        return ans;
+    }
+};
