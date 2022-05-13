ffc536dfa02dd8979dd6953b0f61a967bd11ac5b
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-13 06:28:11 +0530

    Time: 25 ms (49.51%), Space: 25.5 MB (21.91%) - LeetHub

---
+++ 530-minimum-absolute-difference-in-bst/530-minimum-absolute-difference-in-bst.cpp
@@ -0,0 +1,28 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
+#define pb push_back
+#define ppb pop_back
+#define TN TreeNode
+#define LN ListNode
+class Solution {
+public:
+    v<int>v1;
+    int getMinimumDifference(TreeNode* root) {
+        inorder(root);
+        int ans;
+        f(1,v1.size())ans=min(ans,v1[i]-v1[i-1]);
+        return ans;
+    }
+    void inorder(TN *root){
+        if(!root)return;
+        inorder(root->left);
+        v1.pb(root->val);
+        inorder(root->right);
+    }
+};
