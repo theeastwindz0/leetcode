c8024ae15c3489efb09c26cdcc9c5d5607e810ae
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-14 13:41:31 +0530

    Time: 17 ms (53.62%), Space: 14.1 MB (19.86%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,30 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
+#define pb push_back
+#define ppb pop_back
+#define TN TreeNode
+#define LN ListNode
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        um<int,v<int>>um1;
+        v<v<int>> ans;
+        f(0,groupSizes.size()){
+            if(um1.count(groupSizes[i])>0)um1[groupSizes[i]].pb(i);
+            else um1[groupSizes[i]]=v<int>{i};
+            if(um1[groupSizes[i]].size()==groupSizes[i]){
+                ans.pb(um1[groupSizes[i]]);
+                um1.erase(groupSizes[i]);
+            }
+        }
+        
+        return ans;
+        
+    }
+};
