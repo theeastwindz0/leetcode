501d5433468ec5baf6bffc24c3977764f8e596e2
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-16 09:28:53 +0530

    Time: 4 ms (61.12%), Space: 12.1 MB (47.19%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,27 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
+#define pb push_back
+#define ppb pop_back
+#define TN TreeNode
+#define LN ListNode
 class Solution {
 public:
     vector<int>ans;
     vector<int> rightSideView(TreeNode* root) {
-        rl(root,0);
+        if(!root)return {};
+        // rl(root,0);
+        // return ans;
+        queue<TN*>q1;
+        q1.push(root);
+            while(!q1.empty()){
+                int len=q1.size();
+                f(0,len){
+                    TN *curr=q1.front();
+                    q1.pop();
+                    if(i==len-1)ans.pb(curr->val);
+                    if(curr->left)q1.push(curr->left);
+                    if(curr->right)q1.push(curr->right);
+                }
+            }
         return ans;
     }
     
-    void rl(TreeNode *root,int lev){
-        if(root==NULL)return;
-        if(ans.size()==lev)ans.push_back(root->val);
-            rl(root->right,lev+1);
-            rl(root->left,lev+1);
-    }
+    // void rl(TreeNode *root,int lev){
+    //     if(root==NULL)return;
+    //     if(ans.size()==lev)ans.push_back(root->val);
+    //         rl(root->right,lev+1);
+    //         rl(root->left,lev+1);
+    // }
     
 };
