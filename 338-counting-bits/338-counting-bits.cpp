c00d47e0b3ba473d9f8dd134d57bc5e74086e61e
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-18 06:38:50 +0530

    Time: 3 ms (95.60%), Space: 7.8 MB (74.41%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,23 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define p priority_queue
+#define um unordered_map
+#define us unordered_set
+#define pb push_back
+#define ppb pop_back
+#define TN TreeNode
+#define LN ListNode
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans(n+1,0);
+        f(1,n+1){
+            ans[i]=ans[i/2]+i%2;
+        }
+        return ans;
+    }
+};
