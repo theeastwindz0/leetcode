d2367029e1f3bb38b361c7d0acc47ae0da7773dd
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-22 19:19:55 +0530

    Time: 19 ms (32.21%), Space: 21.4 MB (40.70%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -1,36 +1,11 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    // int last=0,sum=0;
-    // bool ans=false;
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        
-//         if(!root|| sum>targetSum){sum-=last; return ans;};
-//         last=root->val;
-//         sum+=root->val;
-//         cout<<sum<<endl;
-//         if(sum==targetSum && !root &&!)ans=true;
-//         hasPathSum(root->left,targetSum);
-//         hasPathSum(root->right,targetSum);
-
-//         return ans;
-        
-        if(!root)return false;
-        if(!root->left && !root->right && targetSum-root->val==0)return true;
-        
-        bool left=hasPathSum(root->left,targetSum-root->val);
-        bool right=hasPathSum(root->right,targetSum-root->val);
-        
-        return left || right;
+    bool ans=0;
+    bool hasPathSum(TreeNode* root, int ts) {
+        if(!root || ans)return ans;
+        if(!root->left && !root->right && ts-root->val==0){ans=1;return ans;}
+        hasPathSum(root->left,ts-root->val);
+        hasPathSum(root->right,ts-root->val);
+        return ans;
     }
 };
