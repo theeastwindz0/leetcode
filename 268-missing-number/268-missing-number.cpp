f02f1734462711220a3bbc002ee0a4d514e8f867
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-28 07:35:59 +0530

    Time: 4 ms (99.97%), Space: 18 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,21 +1,8 @@
-#define ll long long
-#define f(a,b) for(int i=a;i<b;i++)
-#define f2(x,a,b) for(int x=a;x<b;x++)
-#define fr(b,a) for(int i=b;i>=a;i--)
-#define fit(nums) for(auto &it:nums)
-#define v vector
-#define p priority_queue
-#define um unordered_map
-#define pb push_back
-#define ppb pop_back
-#define TN TreeNode
-#define LN ListNode
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        f(0,nums.size())if(i!=nums[i])return i;
-        
-        return nums.size();
+        int ans=0;
+        for(int i=0;i<nums.size();i++)ans^=(i+1)^nums[i];
+        return ans;
     }
 };
