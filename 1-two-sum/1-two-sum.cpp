549c1cb0659667775ba4f85120ecfdeaaf8c1e8e
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-05-31 19:04:36 +0530

    Time: 12 ms (83.99%), Space: 10.7 MB (52.44%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,21 +1,25 @@
+#define ll long long
+#define f(a,b) for(int i=a;i<b;i++)
+#define f2(x,a,b) for(int x=a;x<b;x++)
+#define fr(b,a) for(int i=b;i>=a;i--)
+#define fit(nums) for(auto &it:nums)
+#define v vector
+#define pb push_back
+#define ppb pop_back
+#define p priority_queue
+#define um unordered_map
+#define us unordered_set
+#define TN TreeNode
+#define LN ListNode
+#define endl "\n"
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    
-        vector<int> nums2;
-        
-        for(int i=0;i<nums.size();i++)
-        {
-        for(int j=i+1;j<nums.size();j++)
-        {
-            if(nums[j]+nums[i]==target)
-            {
-                nums2.push_back(i);
-                nums2.push_back(j);
-                break;
-            }     
+        um<int,int>um1;
+        f(0,nums.size()){
+            if(um1.find(target-nums[i])!=um1.end())return{i,um1[target-nums[i]]};
+            um1[nums[i]]=i;
         }
-    }
-        return nums2;
+        return {};
     }
 };
