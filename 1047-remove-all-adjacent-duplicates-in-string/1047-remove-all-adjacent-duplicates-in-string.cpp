f8cabeb3fb1af60d8816c6b4ddba031bcf7864e2
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-06-11 11:37:58 +0530

    Time: 1186 ms (5.05%), Space: 780.9 MB (5.99%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,36 +1,29 @@
-#define ll long long
-#define f(a,b) for(int i=a;i<b;i++)
-#define fr(b,a) for(int i=b;i>=a;i--)
-#define fit(nums) for(auto &it:nums)
-#define v vector
-#define p priority_queue
-#define um unordered_map
-#define pb push_back
-#define TN TreeNode
-#define LN ListNode
 class Solution {
 public:
     string removeDuplicates(string s) {
-        
-//Testcases passed 104/106 Memory limit Exceeded
-
-//             f(1,size(s)){
-//             if(s[i-1]==s[i]){
-//                 if(i==1)s=s.substr(2,size(s));
-//                 else s=s.substr(0,i-1)+s.substr(i+1,size(s));
-                
-//                 if(i==1)i=0;
-//                 else i=i-2;
-//             }
-//         }
-//         cout<<s;
-//         return s;
+      
+        stack<char> st;
         string ans="";
-        fit(s){
-            if(ans.empty() || it!=ans.back())ans.push_back(it);
-            else ans.pop_back();
+    
+        for(int i=0;i<s.length();i++)
+        {
+            if(!st.empty()&&st.top()==s[i])
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
         }
-        return ans;
         
+        while(!st.empty())
+        {
+            ans=st.top()+ans;
+            st.pop();
+        }
+        
+        
+        return ans;
     }
 };
