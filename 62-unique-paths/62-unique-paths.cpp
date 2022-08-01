5edbab1d3891b00a72d3723ce38f4385c1771fb1
Himanshu Chauhan <72276905+theeastwindz0@users.noreply.github.com>
2022-08-01 20:35:13 +0530

    Time: 3 ms (44.81%), Space: 5.9 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int ansFinder(int i,int j,int n,int m, vector<vector<int>>&dp){
-    if(i==(n-1) && j==(m-1))return 1;
-        if(i>=n || j>=m)return 0;
+//     int ansFinder(int i,int j,int n,int m, vector<vector<int>>&dp){
+//     if(i==(n-1) && j==(m-1))return 1;
+//         if(i>=n || j>=m)return 0;
         
-        if(dp[i][j]!=-1)return dp[i][j];
-        else return dp[i][j]=ansFinder(i+1,j,n,m,dp)+ansFinder(i,j+1,n,m,dp);
-    }
+//         if(dp[i][j]!=-1)return dp[i][j];
+//         else return dp[i][j]=ansFinder(i+1,j,n,m,dp)+ansFinder(i,j+1,n,m,dp);
+//     }
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>>v1(n,vector<int>(m,-1));
-        return ansFinder(0,0,n,m,v1);
+        // vector<vector<int>>v1(n,vector<int>(m,-1));
+        // return ansFinder(0,0,n,m,v1);
+        int  N=n+m-2;
+        int r=n-1;
+        double res=1;
+        
+        for(int i=1;i<=r;i++)res=res*(N-r+i)/i;
+        return (int)res;
     }
 };
